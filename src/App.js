dfaf3a4dd67f195cf50127d97e2dc4473e560773
Ifo Ikede <ifoitech@gmail.com>
2022-09-08 06:12:36 -0300

    implement react-countdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import "./Countdown"
+import Countdown ,{ zeroPad, calcTimeDelta, formatTimeDelta } from 'react-countdown';
+
+const renderer = ({ days, hours, minutes, seconds }) => (
+  <span>
+     {zeroPad(days)} Days : {zeroPad(hours)} hours :{zeroPad(minutes)} minutes:{zeroPad(seconds) } seconds
+  </span>
+);
 
 function App() {
   return (
@@ -8,8 +14,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Raye plans to level up by Dec 31st.   They asked us to keep them accountable.
+           so I setup the Raye level up Countdown.
         </p>
+        <Countdown
+        date={new Date("Dec 31 2022" )}
+        intervalDelay={0}
+        precision={3}
+        renderer={renderer}
+        />,
         <a
           className="App-link"
           href="https://reactjs.org"
